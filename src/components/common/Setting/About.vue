713bd640070f36ab6a5c9ced87b018d43ffc060c
venuswjx <wujinxing2006@gmail.com>
2023-03-17 09:22:15 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -43,7 +43,7 @@ onMounted(() => {
           此项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://github.com/venuswjx/chatgpt-web"
             target="_blank"
           >
             Github
